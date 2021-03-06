fd4e9cc148e39fe23d422e0f5d70b49c46c8493d
Jaeyoung Park <jyoung4930@gmail.com>
2021-03-06 13:24:10 +0900

    nodejs-6
    file read

--- for_WEB2-OAuth2.0/Node.js/main.js
+++ for_WEB2-OAuth2.0/Node.js/main.js
@@ -17,7 +17,7 @@ var app = http.createServer(function(request,response){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){ //각 파일이 본문이 description이 되는것.
     var template = `        
     <!doctype html>
     <html>
@@ -33,10 +33,7 @@ var app = http.createServer(function(request,response){
         <li><a href="/?id=JavaScript">JavaScript</a></li>
       </ul>
       <h2>${title}</h2>
-      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-      <img src="coding.jpg" width="100%">
-      </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-      </p>
+      <p>${description}</p> //
     </body>
     </html>
     `;  //변수 받아오는 방법 ${}
@@ -60,3 +57,6 @@ app.listen(3000);
 //시작은 ? 물음표로
 //값 값 & 구분
 //값의 이름과 값은 = 로 구분
+
+//각 파일을 읽어드리는 식으로 하면, 파일내용을 변경하면 바로바로 업데이트 되는걸 확인할 수 있다.
+// main을 다시 실행하시 않아도.
