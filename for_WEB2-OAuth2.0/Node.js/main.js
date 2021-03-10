985f3a5fa4007c14f15d519cdcf433e7be4b2e88
Jaeyoung Park <jyoung4930@gmail.com>
2021-03-10 22:11:56 +0900

    nodejs-10

--- for_WEB2-OAuth2.0/Node.js/main.js
+++ for_WEB2-OAuth2.0/Node.js/main.js
@@ -9,9 +9,9 @@ var app = http.createServer(function(request,response){
     var queryData = url.parse(_url, true).query;    //이 부분이 쿼리스트링에서 받아오는 값을 넣어주는 변수 세팅
     var pathname = url.parse(_url, true).pathname;
     
-    var title = queryData.id;
+    //var title = queryData.id;
     //console.log(queryData.id);
-    console.log(url.parse(_url,true));
+    //console.log(url.parse(_url,true));
     //if(_url == '/'){
     //    title = 'Welcome';
         //_url = '/index.html'; 조건이 / 일때 index.html로 하기 위해 설정
@@ -20,29 +20,60 @@ var app = http.createServer(function(request,response){
     //  return response.writeHead(404);
     //}
     if(pathname === '/'){   //여기가 기본 경로, 다른 경로로 들어올경우, 
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){ //각 파일이 본문이 description이 되는것.
-            var template = `        
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              <ul>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-              </ul>
-              <h2>${title}</h2>
-              <p>${description}</p> //
-            </body>
-            </html>
-            `;  //변수 받아오는 방법 ${}
-            response.writeHead(200);    //200은, 파일을 성공적으로 전송했다
-            response.end(template);
-        });
+        if(queryData.id === undefined){
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){ //각 파일이 본문이 description이 되는것.
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var template = `        
+                <!doctype html>
+                <html>
+                <head>
+                  <title>WEB1 - ${title}</title>
+                  <meta charset="utf-8">
+                </head>
+                <body>
+                  <h1><a href="/">WEB</a></h1>
+                  <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                  </ul>
+                  <h2>${title}</h2>
+                  <p>${description}</p> //
+                </body>
+                </html>
+                `;  //변수 받아오는 방법 ${}
+                response.writeHead(200);    //200은, 파일을 성공적으로 전송했다
+                response.end(template);
+            });
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                var title = queryData.id;
+                var template = `
+                  <!doctype html>
+                  <html>
+                  <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                  </head>
+                  <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ul>
+                      <li><a href="/?id=HTML">HTML</a></li>
+                      <li><a href="/?id=CSS">CSS</a></li>
+                      <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ul>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                  </body>
+                  </html>
+                  `;
+                  response.writeHead(200);
+                  response.end(template);
+                });
+        }
+        
+        
     } else {
         response.writeHead(404);
         response.end('Not found')
