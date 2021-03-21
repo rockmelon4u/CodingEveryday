a44cc0b129033ce05bfbd40686e3088703e03272
Jaeyoung Park <jyoung4930@gmail.com>
2021-03-21 23:10:08 +0900

    Nodejs20 Module

---
+++ for_WEB2-OAuth2.0/Node.js/nodejs/muse.js
@@ -0,0 +1,19 @@
+//var M = {
+//  v:'v',
+//  f:function(){
+//    console.log(this.v);
+//  }
+//}
+ 
+//M.f();
+var part = require('./mpart.js');
+part.f();
+
+/*
+    모듈을 이용하면 이러한 것들을 파일로 쪼개서 
+    웹으로 독립시킬 수 있다.
+    
+    모듈을 가져올때는 require 
+    ./ 현재 디렉도티
+    
+*/
