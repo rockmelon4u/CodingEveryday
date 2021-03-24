0dbeda7c21e75bffadf650bd22eb99e892ce8b94
Jaeyoung Park <jyoung4930@gmail.com>
2021-03-25 08:02:02 +0900

    nodejs express 2
    page

--- for_WEB2-OAuth2.0/express/main.js
+++ for_WEB2-OAuth2.0/express/main.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var app = express()
-var fs = require('fs');
-var template = require('./lib/template.js');
+const fs = require('fs');
+const { request } = require('http');
+const sanitizeHtml = require('sanitize-html');
+const template = require('./lib/template.js');
  
 //route, routing
 //app.get('/', (req, res) => res.send('Hello World!'))
@@ -18,10 +20,34 @@ app.get('/', function(request, response) {
   });
 });
  
-app.get('/page', function(req, res) { 
-  return res.send('/page');
+app.get('/page/:pageId', function(request, response) { 
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update?id=${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    });
+  });
 });
- 
+
+app.get('/page/:pageId', function(request, response){
+  response.send(request.params);
+});
+
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
 });
