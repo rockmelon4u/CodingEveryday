99a67755aa2a46c587647063f1c55736f4ed9e6b
Jaeyoung Park <jyoung4930@gmail.com>
2021-04-18 23:05:56 +0900

    nodejs express11 routing
    라우팅은 애플리케이션의 엔드 포인트URI가 클라이언트 요청에 응답하는 방식

--- for_WEB2-OAuth2.0/express/main.js
+++ for_WEB2-OAuth2.0/express/main.js
@@ -38,46 +38,16 @@ app.get('/', function(request, response) {
   var html = template.HTML(title, list,
     `<h2>${title}</h2>${description},
     <img src="/images/hello.jpg" style="width:300px; display:block"></img>`,
-    `<a href="/create">create</a>`
+    `<a href="/topic/create">create</a>`
   ); 
   response.send(html);
 });
 
-app.get('/page/:pageId', function(request, response, next) { 
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-    if(err){
-      next(err);
-    } else {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
-      });
-      var list = template.list(request.list);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-          /*<form action="/delete_process" method="post">
-                          / 위처럼, 여기에 슬러시를 주면
-                          클릭했을때 최상위 밑의
-          */
-      );
-      response.send(html);
-    }
-  });
-});
-
-app.get('/create', function(request, response){
+app.get('/topic/create', function(request, response){
   var title = 'WEB - create';
   var list = template.list(request.list);
   var html = template.HTML(title, list, `
-    <form action="/create_process" method="post">
+    <form action="/topic/create_process" method="post">
       <p><input type="text" name="title" placeholder="title"></p>
       <p>
         <textarea name="description" placeholder="description"></textarea>
@@ -90,7 +60,7 @@ app.get('/create', function(request, response){
   response.send(html);
 });
 
-app.post('/create_process', function(request, response){
+app.post('/topic/create_process', function(request, response){
 
   /*
   var body = '';
@@ -111,19 +81,19 @@ app.post('/create_process', function(request, response){
   var title = post.title;
   var description = post.description;
   fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-    response.writeHead(302, {Location: `/?id=${title}`});
+    response.writeHead(302, {Location: `/topic/${title}`});
     response.end();
   })
 });
 
-app.get('/update/:pageId', function(request, response){
+app.get('/topic/update/:pageId', function(request, response){
   var filteredId = path.parse(request.params.pageId).base;
   fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
     var title = request.params.pageId;
     var list = template.list(request.list);
     var html = template.HTML(title, list,
       `
-      <form action="/update_process" method="post">
+      <form action="/topic/update_process" method="post">
         <input type="hidden" name="id" value="${title}">
         <p><input type="text" name="title" placeholder="title" value="${title}"></p>
         <p>
@@ -134,14 +104,14 @@ app.get('/update/:pageId', function(request, response){
         </p>
       </form>
       `,
-      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
     );
     //위의 코드에서 /update?id=${title} 부분은 /update/${title}로 수정 되어야 하는 버그입니다. 
     response.send(html);
   });
 });
  
-app.post('/update_process', function(request, response){
+app.post('/topic/update_process', function(request, response){
   /*var body = '';
   request.on('data', function(data){
       body = body + data;
@@ -170,7 +140,7 @@ app.post('/update_process', function(request, response){
 
 });
 
-app.post('/delete_process', function(request, response){
+app.post('/topic/delete_process', function(request, response){
   /*
   var body = '';
   request.on('data', function(data){
@@ -196,6 +166,38 @@ app.post('/delete_process', function(request, response){
     response.redirect('/');
   });
 })
+
+app.get('/topic/:pageId', function(request, response, next) { 
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+    if(err){
+      next(err);
+    } else {
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(request.list);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/topic/create">create</a>
+          <a href="/topic/update/${sanitizedTitle}">update</a>
+          <form action="/topic/delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+          /*<form action="/delete_process" method="post">
+                          / 위처럼, 여기에 슬러시를 주면
+                          클릭했을때 최상위 밑의
+          */
+      );
+      response.send(html);
+    }
+  });
+});
+
+
 
 app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that!');
