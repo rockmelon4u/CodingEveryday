85f6f5b698ea2b047be3903f6cb92518e5d83606
Jaeyoung Park <jyoung4930@gmail.com>
2021-04-19 17:25:45 +0900

    nodejs express12 files-2

--- for_WEB2-OAuth2.0/express/main.js
+++ for_WEB2-OAuth2.0/express/main.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
 var fs = require('fs');
-var template = require('./lib/template.js');
+
 var qs = require('querystring');
 var bodyParser = require('body-parser')
 
 //라우터로 파일분리
+var indexrouter = require('./routes/index');
 var topicRouter = require('./routes/topic');
 
 //static files
@@ -22,35 +23,21 @@ app.use(compression());
 
 //리스트를 호출하는 미들웨어를 만들어보자
 //app.use(function(request, response, next){
-app.get('*', function(request, response, next){
-  fs.readdir('./data', function(error, filelist){
+app.get('*', function (request, response, next) {
+  fs.readdir('./data', function (error, filelist) {
     request.list = filelist;
     next();
   });
 });
 
+app.use('/', indexrouter);
 app.use('/topic', topicRouter);
 // /topic으로 시작하는 주소 이름은 미드웨어를 적용하겠다라는 의미.
 
 //route, routing
 //app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/', function(request, response) { 
-  //console(request.list);
-  var title = 'Welcome';
-  var description = 'Hello, Node.js';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list,
-    `<h2>${title}</h2>${description},
-    <img src="/images/hello.jpg" style="width:300px; display:block"></img>`,
-    `<a href="/topic/create">create</a>`
-  ); 
-  response.send(html);
-});
 
-
-
-
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404).send('Sorry cant find that!');
 });
 
@@ -59,6 +46,6 @@ app.use(function (err, req, res, next) {
   res.status(500).send('Something broke!')
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
---
+++ for_WEB2-OAuth2.0/express/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var template = require('../lib/template.js');
+
+router.get('/', function (request, response) {
+    //console(request.list);
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+        `<h2>${title}</h2>${description},
+      <img src="/images/hello.jpg" style="width:300px; display:block"></img>`,
+        `<a href="/topic/create">create</a>`
+    );
+    response.send(html);
+});
+
+module.exports = router;
