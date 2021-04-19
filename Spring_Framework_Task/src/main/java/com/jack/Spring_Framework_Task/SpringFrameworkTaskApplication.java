aec1ef0101f7c1706d2fca1d16bb4201a54fe476
Jaeyoung Park <jyoung4930@gmail.com>
2021-04-20 08:37:50 +0900

    Spring_Framework_Task 01

---
+++ Spring_Framework_Task/src/main/java/com/jack/Spring_Framework_Task/SpringFrameworkTaskApplication.java
@@ -0,0 +1,13 @@
+package com.jack.Spring_Framework_Task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkTaskApplication.class, args);
+	}
+
+}
---
+++ Spring_Framework_Task/src/test/java/com/jack/Spring_Framework_Task/SpringFrameworkTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.Spring_Framework_Task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
