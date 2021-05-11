860e352ce0ce67e87d2a3f5fe9ef84ba51c1f1ea
Jaeyoung Park <jyoung4930@gmail.com>
2021-05-11 14:34:40 +0900

    interface 4 cooperation_1
    아무런 협약없이 만들기 시작.
    이렇게 만들게 되면,
    메서드명, 클래스명 너무도 다르다.
    나중에 이 클래스명을 받고 개발을 진행하게 된다면..

---
+++ InterfacePractice/src/com/jack/cla/EnglishTrans.java
@@ -0,0 +1,13 @@
+package com.jack.cla;
+
+public class EnglishTrans {
+	public String koreanToEnglish(String str) {
+		//Todo: 한국어를 영어로 번역
+		return str;
+	}
+	
+	public String englishToKorean(String str) {
+		//Todo: 영어를 한국어로 번역
+		return str;
+	}
+}
---
+++ InterfacePractice/src/com/jack/cla/Japaneshonnyaku.java
@@ -0,0 +1,13 @@
+package com.jack.cla;
+
+public class Japaneshonnyaku {
+	public String translateJapanese(String str) {
+		//Todo: 한국어를 일본어로 번역
+		return str;
+	}
+	
+	public String translateKorean(String str) {
+		//Todo: 일본어를 한국어로 번역
+		return str;
+	}
+}
