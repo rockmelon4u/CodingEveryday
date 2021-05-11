c00d8c54d3ee483d3008837a222299bf679d4a59
Jaeyoung Park <jyoung4930@gmail.com>
2021-05-11 14:30:28 +0900

    interface 3 구현

---
+++ InterfacePractice/src/com/jack/cla/Animals.java
@@ -0,0 +1,11 @@
+package com.jack.cla;
+
+import com.jack.interf.Animal;
+
+public class Animals implements Animal{
+	
+	@Override
+	public void eat() {
+		System.out.println("ธิดยดู");
+	}
+}
---
+++ InterfacePractice/src/com/jack/cla/Main.java
@@ -0,0 +1,10 @@
+package com.jack.cla;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		Animals ani = new Animals();
+		
+	}
+
+}
